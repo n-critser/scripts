a04167ee470bf766e730f09f66e6d51cfe3ee823
n-critser <critser101@gmail.com>
2014-02-03 23:15:50 -0500

    changed forToday.sh

--- forToday.sh
+++ forToday.sh
@@ -16,8 +16,8 @@ clear
 TODAY="`date +"%m"`.`date +"%d"`.`date +"%y"`"
 #/home/chaos/Desktop/DoThisShit
 SEARCH_DIR=/home/"$USER"/dayIsDone
-SEARCH_PATH=$SEARCH_DIR/$TODAY
 TODAY_FILE="$TODAY.org"
+SEARCH_PATH=$SEARCH_DIR/$TODAY_FILE
 echo "$SEARCH_DIR"
 echo "$SEARCH_PATH"
 echo "$TODAY"
@@ -34,10 +34,21 @@ if [ -f $SEARCH_PATH ];
     echo "$SEARCH_PATH"
     echo "$TODAY_FILE"
     echo "$TODAY"
+    echo "** " >> "$TODAY_FILE"
     cal `date +"%m"` `date +"%Y"`
-    more "$TODAY_FILE"
+    #more "$TODAY_FILE"
     emacs "$TODAY_FILE" -nw
   else
+    #TIME = 
+    echo "#+STARTUP: overview" >> "$TODAY_FILE"
+    echo "#+AUTHOR: n-critser" >> "$TODAY_FILE"
+    echo "#+TITLE: $TODAY_FILE" >> "$TODAY_FILE"
+    echo "*`zdump EST`*" >> "$TODAY_FILE"
+    echo "" >> "$TODAY_FILE" 
+    echo "C-c C-x C-i  == org-clock-in " >> "$TODAY_FILE"
+    echo "C-c C-x C-o   == org-clock-out " >> "$TODAY_FILE"
+    echo "* Tasks hit C-c C-t" >> "$TODAY_FILE"
+    echo "** TODO  This Shit" >> "$TODAY_FILE" 
     emacs "$TODAY_FILE" -nw 
     
 fi
